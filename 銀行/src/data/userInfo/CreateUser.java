b448f21058504d801a9080aa1629de49f050837d
shohe <shohe@192.168.10.102>
2025-07-06 20:47:08 +0900

    修正プッシュ

--- 銀行/src/data/userInfo/CreateUser.java
+++ 銀行/src/data/userInfo/CreateUser.java
@@ -12,7 +12,11 @@ public class CreateUser {
 		
 		while(true) {
 			System.out.println("姓を漢字、またはカタカナで入力してください");
+			System.out.println("終了するには\"esc\"と入力してください");
 			lastname = sc.nextLine();
+			if(lastname.contains("esc")) {
+				return;
+			}
 			if(lastname.matches("[\\p{IsHan}\\u30A0-\\u30FF]+")) {
 				break;
 			}else {
@@ -42,20 +46,29 @@ public class CreateUser {
 		String email = "";
 		while(true) {
 			System.out.println("メールアドレスを追加してください");
+			System.out.println("終了するには\"esc\"と入力してください");
 			email = sc.nextLine();
-			if(email.contains("@")) {
-				break;
-			}else{
-				System.out.println("正しいアドレスを入力してください");
+			if(email.contains("esc")) {
+				return;
 			}
+			if(!email.contains("@")) {
+				System.out.println("正しいアドレスを入力してください");
+				continue;
+			}else{
+			if(isEmailTaken(email)) {
+				System.out.println("このメールアドレスはすでに使用されています");
+				continue;
+			}
+			break;
 		}
-		sc.close();
+		}
+		String cardnumber = CreateCardNumber.GenerateCardNum();
 		
-		addUserToDatabase(lastname, firstname, pin, email);
+		addUserToDatabase(lastname, firstname, pin, email, cardnumber);
 		}
 	
-		private static void addUserToDatabase(String lastname, String firstname, String pin, String email) {
-			String sql = "INSERT INTO users(lastname, firstname, pin, email) VALUES(?,?,?,?)";
+		private static void addUserToDatabase(String lastname, String firstname, String pin, String email, String cardnumber) {
+			String sql = "INSERT INTO users(lastname, firstname, pin, email, cardnumber) VALUES(?,?,?,?,?)";
 			
 			try (Connection con = DBConnection.getConnection();
 					PreparedStatement ps = con.prepareStatement(sql)){
@@ -64,13 +77,27 @@ public class CreateUser {
 				ps.setString(2, firstname);
 				ps.setString(3, pin);
 				ps.setString(4, email);
+				ps.setString(5, cardnumber);
 				
 				int result = ps.executeUpdate();
 				if(result>0) {
 					System.out.println("ユーザーが作成されました");
+					System.out.println("貴方のカード番号は "+cardnumber+" になります");
 				}
 			}catch (Exception e) {
 				e.printStackTrace();
 			}	
 	}
+		private static boolean isEmailTaken(String email) {
+			String sql = "SELECT 1 FROM users WHERE email = ?";
+			try(Connection con = DBConnection.getConnection();
+					PreparedStatement ps = con.prepareStatement(sql)){
+				
+				ps.setString(1, email);
+				return ps.executeQuery().next();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			return false;
+		}
 }
---
+++ 銀行/src/data/userInfo/forgotInfo.java
@@ -0,0 +1,8 @@
+package data.userInfo;
+
+public class forgotInfo {
+	//forgot password
+	//forgot cardnumber
+	//send verification code to email
+	//forgot email - 管理者権限持ちの人が操作
+}
